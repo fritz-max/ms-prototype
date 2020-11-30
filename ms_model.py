28605fdff6e49eba77a4d4ad74ff9668d411abae
fritz-max <fritz.doermann@gmail.com>
2020-11-30 18:07:56 +0100

    Added caller and callee topics

--- ms_model.py
+++ ms_model.py
@@ -9,7 +9,7 @@ class MicroService:
         self.router_url = "ws://crossbar_router:8080/ws"
         self.router_realm = "realm"
 
-        subs, pubs = {}, {}
+        subs, pubs, caller, callee = {}, {}, {}, {}
 
         for topic in model['subscribe-topics']:
             subs[topic] = f"com.airvisor/{topic}"
@@ -17,8 +17,16 @@ class MicroService:
         for topic in model['publish-topics']:
             pubs[topic] = f"com.airvisor/{topic}"
 
+        for topic in model['caller-topics']:
+            caller[topic] = f"com.airvisor/{topic}"
+
+        for topic in model['callee-topics']:
+            callee[topic] = f"com.airvisor/{topic}"
+
         self.subscribe_topics = subs
         self.publish_topics = pubs
+        self.caller_topics = caller
+        self.callee_topics = callee
 
 def load_ms_model(path='model.json'):
     return MicroService(path)
